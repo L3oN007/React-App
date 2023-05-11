11033dbe91251c6e4b07dece2b0bf2369d6522a5
Minh Nhật <89580650+L3oN007@users.noreply.github.com>
2023-05-11 21:37:28 +0700

    improve CSS

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,29 +3,33 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
   render() {
     return (
-        <footer>
-        <div class="container">
-          <div class="row">
+      <footer>
+    <div class="container">
+        <div class="row">
             <div class="col">
-              <h4>About Us</h4>
-              <p>We are a company that specializes in...</p>
+                <h4>About Us</h4>
+                <p>We are a company that specializes in...</p>
             </div>
             <div class="col">
-              <h4>Contact Us</h4>
-              <p>Email: info@company.com</p>
-              <p>Phone: (123) 456-7890</p>
+                <h4>Contact Us</h4>
+                <p>Email: info@company.com</p>
+                <p>Phone: (123) 456-7890</p>
             </div>
             <div class="col">
-              <h4>Follow Us</h4>
-              <ul class="social-icons">
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-              </ul>
+                <h4>Follow Us</h4>
+                <p>Facebook</p>
+                <p>Twitter</p>
+                <p>Instagram</p>
             </div>
-          </div>
-        </div>
-      </footer>
+        </div>     
+    </div>
+    
+    <div class="footer-bottom">
+        <p>&copy; 2023, FER201m - Vu Minh Nhat</p>
+    </div>
+</footer>
+
+    
       
     )
   }
